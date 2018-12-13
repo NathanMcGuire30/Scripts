6e6eb19911113e6d3815de85e7b8f908d7ce393b
nathan <nathan.e.mcguire@gmail.com>
2018-12-13 18:12:08 -0500

    conky fixes

---
+++ conky-startup.sh
@@ -0,0 +1,16 @@
+#sleep 20s
+killall conky
+#cd "/home/nathan/.conky"
+conky -c "/home/nathan/Documents/Scripts/Conky/Calender" &
+#cd "/home/nathan/.conky"
+conky -c "/home/nathan/Documents/Scripts/Conky/Graphics" &
+#cd "/home/nathan/.conky"
+conky -c "/home/nathan/Documents/Scripts/Conky/Network" &
+#cd "/home/nathan/.conky"
+#conky -c "/home/nathan/Documents/Scripts/Conky/Simpleclock" &
+#cd "/home/nathan/.conky"
+conky -c "/home/nathan/Documents/Scripts/Conky/Large_clock" &
+#cd "/home/nathan/.conky"
+conky -c "/home/nathan/Documents/Scripts/Conky/Weather" &
+#cd "/home/nathan/.conky"
+#conky -c "/home/nathan/Documents/Scripts/Conky/WeatherSmall" &
