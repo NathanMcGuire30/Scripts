4ac90a7090b28adadb4e84c8835ed7bfc601beca
Nathan McGuire <nathan.e.mcguire@gmail.com>
2021-03-26 15:11:26 -0400

    Update for new computer install

--- Startup.bat
+++ Startup.bat
@@ -1,9 +1,9 @@
-call C:\Users\natha\Documents\Scripts\copyKSPScreenshots.bat
+::call C:\Users\natha\Documents\Scripts\copyKSPScreenshots.bat
 
-call C:\Users\natha\Documents\Scripts\CopyMinecraftMods.bat
+::call C:\Users\natha\Documents\Scripts\CopyMinecraftMods.bat
 
-call C:\Users\natha\Documents\Scripts\CopyMinecraftScreenshots.bat
+::call C:\Users\natha\Documents\Scripts\CopyMinecraftScreenshots.bat
 
-call C:\Users\natha\Documents\Scripts\CopyMinecraftResourcePacks.bat
+::call C:\Users\natha\Documents\Scripts\CopyMinecraftResourcePacks.bat
 
-call C:\Users\natha\AppData\Local\Programs\Python\Python38-32\python.exe C:\Users\natha\Documents\Scripts\sortPics.py C:\Users\natha\Pictures\misc\ C:\Users\natha\Downloads\
+call C:\Users\natha\AppData\Local\Programs\Python\Python38\python.exe C:\Users\natha\Documents\Scripts\sortPics.py C:\Users\natha\Pictures\misc\ C:\Users\natha\Downloads\
